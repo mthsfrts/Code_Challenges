adb85c19c0a1928fcca27f485c3380109f1f014a
Matheus Freitas <105947137+mthsfrts@users.noreply.github.com>
2022-09-03 20:54:45 -0300

    Update Array_Challenge.py

--- BordelessHr/Array_Challenge.py
+++ BordelessHr/Array_Challenge.py
@@ -1,45 +1,48 @@
 # This challenge was a part of an assessment series from BorderlessHR Company.
 
-# Array Challenge
+"""
+Difficulty: Hard
+Description: For this challenge you will determine how many times you can jump around within an array.
 
-# Difficulty: Hard
-# Description: For this challenge you will determine how many times you can jump around within an array.
-#
-# Have the function ArrayChallenge(arr) take the array of numbers stored in arr and first determine the largest
-# element in the array, and then determine whether you can reach that same element within the array by moving left or
-# right continuously according to whatever integer is in the current spot. If you can reach the same spot within the
-# array, then your program should output the least amount of jumps it took.
-#
-# For example: if the input is [2, 3, 5, 6, 1] you'll start at the spot where 6 is and if you jump 6 spaces to the right
-# while looping around the array you end up at the last element where the 1 is.
-#
-# Then from here you jump 1 space to the left, and you're back where you started, so your program should output 2.
-# If it's impossible to end up back at the largest element in the array your program should output -1.
-#
-# The largest element in the array will never equal the number of elements in the array. The largest element will be
-# unique.
-#
-# Input:1,2,3,4,2
-#
-# Output:3
-#
-# Input:1,7,1,1,1,1
-#
-# Output:2
-# ___________________________________________________________________________________________________________________
-# Assumptions
+___________________________________________________________________________________________________________________
+Challenge:
 
-# Assume largest element is unique
-# Largest element doesn't equal len(arr) > output > 1
-# Positive integers only
-# Number of Jumps is equal to the largest element
+Have the function ArrayChallenge(arr) take the array of numbers stored in arr and first determine the largest
+element in the array, and then determine whether you can reach that same element within the array by moving left or
+right continuously according to whatever integer is in the current spot. If you can reach the same spot within the
+array, then your program should output the least amount of jumps it took.
+
+For example: if the input is [2, 3, 5, 6, 1] you'll start at the spot where 6 is and if you jump 6 spaces to the right
+while looping around the array you end up at the last element where the 1 is.
+
+Then from here you jump 1 space to the left, and you're back where you started, so your program should output 2.
+If it's impossible to end up back at the largest element in the array your program should output -1.
+
+The largest element in the array will never equal the number of elements in the array. The largest element will be
+unique.
+
+Input:1,2,3,4,2
+
+Output:3
+
+Input:1,7,1,1,1,1
+
+Output:2
+___________________________________________________________________________________________________________________
+Assumptions
+
+Assume largest element is unique
+Largest element doesn't equal len(arr) > output > 1
+Positive integers only
+Number of Jumps is equal to the largest element
 
 
-# Code Output
+Code Output
 
-# The least amount of jumps to reach that same element within the array by moving left or
-# right continuously according to whatever integer is in the current spot
-# ___________________________________________________________________________________________________________________
+The least amount of jumps to reach that same element within the array by moving left or
+right continuously according to whatever integer is in the current spot
+___________________________________________________________________________________________________________________
+"""
 
 # Here is my solution for this particular one.
 
